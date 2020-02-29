73ed540909db040ae8fb402c7fd180bd5260f668
Naruto-sys <gavriil.belov@yandex.ru>
2020-02-29 21:44:58 +0300

    Start working on Firebase

--- app/src/main/java/com/example/mymiptapp/Controllers/AuthController.java
+++ app/src/main/java/com/example/mymiptapp/Controllers/AuthController.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -13,6 +14,10 @@ import com.bluelinelabs.conductor.Controller;
 import com.bluelinelabs.conductor.RouterTransaction;
 import com.bluelinelabs.conductor.changehandler.FadeChangeHandler;
 import com.example.mymiptapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.net.URI;
 
@@ -33,7 +38,27 @@ public class AuthController extends Controller {
         ButterKnife.bind(this, view);
         textLogin = view.findViewById(R.id.login);
         textPassword = view.findViewById(R.id.login);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // Already signed in
+            // Do nothing
+        }
         return view;
+    }
+
+    @OnClick({R.id.authButton}) void launchAuth() {
+        String log = ((EditText)getView().findViewById(R.id.login)).getText().toString();
+        String pas = ((EditText)getView().findViewById(R.id.password)).getText().toString();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(log, pas)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            getRouter().pushController(new LoginController());
+                        }
+                    }
+                });
     }
 
     private boolean isPasswordValid(String text) {
